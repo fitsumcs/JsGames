7d705eabd1a0f4b2c27c368425d90692ecd85471
3020 <fitsumcs@gmail.com>
2020-07-22 16:49:40 +0300

    Adding Event Listener in Imgs

--- rock_paper_sesors/app.js
+++ rock_paper_sesors/app.js
@@ -1,5 +1,32 @@
-document.addEventListener('DOMContentLoaded', loadModal);
+// selecet images
+const imgs = document.querySelectorAll('img');
+
+// add action listener 
+imgs.forEach((img) => img.addEventListener('click', myfun));
+
+
+function myfun(e) {
+    console.log(e.target.id);
+    // loadModal();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function loadModal() {
-    // $('#exampleModalCenter').modal('show');
+    $('#exampleModalCenter').modal('show');
 }
