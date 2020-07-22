1c04621d1065ccfe392e2013d85952f5f3615c15
3020 <fitsumcs@gmail.com>
2020-07-22 17:18:26 +0300

    Adding the random logic

--- rock_paper_sesors/app.js
+++ rock_paper_sesors/app.js
@@ -1,13 +1,59 @@
 // selecet images
 const imgs = document.querySelectorAll('img');
+const span = document.querySelectorAll('span');
+
+let score = {
+    player: 0,
+    compuer: 0
+};
+
+span[0].innerText = score.player;
+span[1].innerText = score.compuer;
 
 // add action listener 
 imgs.forEach((img) => img.addEventListener('click', myfun));
 
 
 function myfun(e) {
+
+    const userchoice = e.target.id;
+    const compuerchoice = randomechoice();
+    const whowhins = winner(userchoice, compuerchoice);
     console.log(e.target.id);
+    score.player++;
+    score.compuer++;
+    span[0].innerText = score.player;
+    span[1].innerText = score.compuer;
     // loadModal();
+}
+
+// random computer choice 
+function randomechoice() {
+
+    const rand = Math.random() * 100;
+    let choice = '';
+    if (rand > 0) {
+        choice = "paper";
+
+    }
+    if (rand > 30) {
+        choice = "scissors";
+
+    }
+    if (rand > 60) {
+        choice = "rock";
+
+    }
+
+    console.log(choice);
+
+
+
+}
+
+// who wins 
+function winner(userchoice, compuerchoice) {
+
 }
 
 
